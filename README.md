9f83fd1f4aad846a833f2033e7f1546301f8a0f6
marcaroni3d <marc.eddington@gmail.com>
2024-01-20 18:25:45 -0500

    version 2.0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # API Test
 
+A simple gif searcher app, to practice API fetching.
+
 [Live Demo](https://marcaroni3d.github.io/api-test)
--- script.js
+++ script.js
@@ -1,28 +1,43 @@
+// ELEMENTS
 const img = document.querySelector('img')
-const button = document.querySelector('button')
-const loadingText = document.querySelector('.loading-text')
+const newImageButton = document.querySelector('.new-image-button')
+const searchButton = document.querySelector('.search-button')
+const searchInput = document.querySelector('.search-input')
+const searchDisplay = document.querySelector('.search-display')
 
-function fetchCats() {
-    updateLoadingText()
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=zhtTBSfywHzSGo8tDczGNW4T7rNgJ0pz&s=cats', {mode: 'cors'})
+let searchString = ''
+const defaultSearch = 'cats'
+
+// LISTENERS
+newImageButton.onclick = fetchSearch
+searchButton.onclick = handleSearch
+searchInput.onkeydown = (e) => { if (e.keyCode === 13) handleSearch() }
+window.onload = init()
+
+// FUNCTIONS
+function init() {
+    searchString = defaultSearch
+    searchDisplay.innerHTML = searchString
+    searchInput.value = ''
+    fetchSearch()
+}
+
+function handleSearch() {
+    searchString = searchInput.value
+    searchDisplay.innerHTML = searchString
+    fetchSearch()
+}
+
+function fetchSearch() {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zhtTBSfywHzSGo8tDczGNW4T7rNgJ0pz&s=${searchString}`, {mode: 'cors'})
     .then(function(response) {
         return response.json()
     })
     .then(function(response) {
         img.src = response.data.images.original.url
-        setTimeout(updateLoadingText, 500)
     });
+
 }
 
-function updateLoadingText() {
-    if (loadingText.textContent == null || loadingText.textContent == undefined || loadingText.textContent == '') {
-        loadingText.textContent = 'Loading...'
-    }
-    else {
-        loadingText.textContent = ''
-    }
-}
 
-button.onclick = fetchCats
 
-fetchCats()
