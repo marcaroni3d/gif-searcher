ab77ffbf866eb19ad2ab9ff83a794d977b8e7b6c
marcaroni3d <marc.eddington@gmail.com>
2024-01-21 11:16:31 -0500

    version 2.1: loading animation

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const newImageButton = document.querySelector('.new-image-button')
 const searchButton = document.querySelector('.search-button')
 const searchInput = document.querySelector('.search-input')
 const searchDisplay = document.querySelector('.search-display')
+const loader = document.querySelector('.loading')
 
 let searchString = ''
 const defaultSearch = 'cats'
 
 // LISTENERS
-newImageButton.onclick = fetchSearch
+newImageButton.onclick = fetchHandler
 searchButton.onclick = handleSearch
 searchInput.onkeydown = (e) => { if (e.keyCode === 13) handleSearch() }
 window.onload = init()
@@ -19,25 +20,33 @@ function init() {
     searchString = defaultSearch
     searchDisplay.innerHTML = searchString
     searchInput.value = ''
-    fetchSearch()
+    fetchHandler()
 }
 
 function handleSearch() {
     searchString = searchInput.value
     searchDisplay.innerHTML = searchString
-    fetchSearch()
+    fetchHandler()
 }
 
-function fetchSearch() {
+function fetchHandler() {
+    displayLoading()
+
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zhtTBSfywHzSGo8tDczGNW4T7rNgJ0pz&s=${searchString}`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json()
-    })
+    .then(response => response.json())
     .then(function(response) {
         img.src = response.data.images.original.url
+        hideLoading()
     });
-
 }
 
+function displayLoading() {
+    loader.classList.add('display')
+    setTimeout(() => {
+        loader.classList.remove('display')
+    }, 5000)
+}
 
-
+function hideLoading() {
+    loader.classList.remove('display')
+}
